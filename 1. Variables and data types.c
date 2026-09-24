7f014eae6d19f95711798fbf73ad53f3b4f899a5
Pavan123-creator <pavankumarraju1999@gmail.com>
2026-09-24 15:06:25 +0530

    Add files via upload
    Add First c program

---
+++ 1. Variables and data types.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // x,y,z and ch are variables
+    // int, float, double, char are data types
+    // double provides more precision than float
+    int x = 2;
+    float y = 98.123456f;
+    double z = 98.123456;
+    char ch = 'A';
+    printf("Integer: %d\n", x);
+    printf("Float: %f\n", y);
+    printf("Double: %lf\n", z);
+    printf("Character: %c\n", ch);
+
+    return 0;
+}
